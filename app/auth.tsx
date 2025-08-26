819f9cf55bc3dbe3b84eae8cb67f7f514d23abd6
nhwnahid007 <nhwnahid@gmail.com>
2025-08-26 13:52:28 +0600

    refactor: enhance AuthScreen layout and styling for better user experience

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 
 import { Button, Text, TextInput } from "react-native-paper";
 export default function AuthScreen() {
@@ -8,12 +8,17 @@ export default function AuthScreen() {
   const handleSwitchMode = () => {
     setIsSignUp((prev) => !prev);
   };
+  const handleAuth = () => {};
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
     >
-      <View>
-        <Text> {isSignUp ? "Create Account" : "Welcome Back"}</Text>
+      <View style={styles.content}>
+        <Text style={styles.title} variant="headlineMedium">
+          {" "}
+          {isSignUp ? "Create Account" : "Welcome Back"}
+        </Text>
 
         <TextInput
           label="Email"
@@ -21,18 +26,54 @@ export default function AuthScreen() {
           keyboardType="email-address"
           placeholder="example@gmail.com"
           mode="outlined"
+          style={styles.input}
         />
         <TextInput
           label="Password"
           autoCapitalize="none"
           keyboardType="email-address"
           mode="outlined"
+          m
+          style={styles.input}
         />
+        <Button style={styles.button} mode="contained" onPress={handleAuth}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+        <Button
+          style={styles.switchModeButton}
+          mode="text"
+          onPress={handleSwitchMode}
+        >
+          {isSignUp
+            ? "Already have an account"
+            : "Dont have an account? Sign Up"}
+        </Button>
       </View>
-      <Button mode="contained">{isSignUp ? "Sign Up" : "Sign In"}</Button>
-      <Button mode="text" onPress={handleSwitchMode}>
-        {isSignUp ? "Already have an account" : "Dont have an account? Sign Up"}
-      </Button>
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
